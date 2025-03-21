0415ebd3a86d011118e78ebd1dface44d35d425d
alinosi <146401086+alinosi@users.noreply.github.com>
2025-03-21 22:21:04 +0700

    @

--- routes/web.php
+++ routes/web.php
@@ -2,19 +2,23 @@
 
 use Illuminate\Support\Facades\Route;
 
+function getuserdata() {
+    return $data = [
+        'name' => 'M Thoriqul Fadli',
+        'email' => 'mthoriqulfadli@gmal.com',
+        'NIM' => '09031282328037'
+    ];
+}
+
 Route::get('/', function () {
     return view('welcome');
 });
 
 Route::get('/home', function () {
-    return view('home', [
-        'name' => 'M Thoriqul Fadli',
-        'email' => 'mthoriqulfadli@gmal.com',
-        'NIM' => '09031282328037'
-    ] );
+    return view('home', getuserdata());
 });
-
 
 Route::get('login', function () {
     return view('account/login');
 });
+
