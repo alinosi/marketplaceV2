0160d52ed396bffdda81884f3da5f10a2f5a6ef5
alinosi <146401086+alinosi@users.noreply.github.com>
2025-04-13 22:48:54 +0700

    Add Guard contract import to User model; clean up commented code

--- app/Models/User.php
+++ app/Models/User.php
@@ -3,6 +3,8 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
+
+use Illuminate\Contracts\Auth\Guard;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -22,6 +24,11 @@ class User extends Authenticatable
         'email',
         'password',
     ];
+
+    /** protected $guarded = [
+     *     'id',
+     *  ];
+     */ 
 
     /**
      * The attributes that should be hidden for serialization.
